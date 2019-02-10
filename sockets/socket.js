a12d300a757176e212ba585139dcda0a12a9f891
CornelSora <cornelsora@gmail.com>
2019-02-10 18:36:10 +0200

    new commit

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,3 +1,4 @@
+try {
 var express = require('express')
 var app = express();
 const cmd = require('node-cmd');
@@ -11,6 +12,7 @@ app.use(bodyParser());
 const debugStarted = {};
 
 io.on('connection', function(socket){
+    console.warn(`socket connected: ${socket.id}`)
     var processRef
     socket.on('disconnect', () => {
         fs.unlink(`${socket.id}.c`, (data, err) => {
@@ -20,26 +22,69 @@ io.on('connection', function(socket){
     })
     socket.on('debugStart', () => {
         let i = 0
-        processRef = cmd.get(`gdb --quiet ${socket.id}.exe`);
+        var command = `gdb --quiet ${socket.id}.exe`
+        console.warn(command)
+        processRef = cmd.get(command);
         debugStarted[socket.id] = true
         processRef.stdout.on(
             'data',
             function(data) {
+                console.log(data)
                 if (data.indexOf("exited") > -1) {
                     debugStarted[socket.id] = false
                 }
-                socket.emit("debugResult", data)
+                try {
+                    socket.emit("debugResult", data)
+                } catch (e) {
+                    console.log(e)
+                }
             }
         );
     })
 
     socket.on("debug", function(command) {
-        if (command === 'next' || command === 'continue') {
-            socket.emit("next", true)
-        }
+        // if (command === 'next' || command === 'continue') {
+        //     socket.emit("next", true)
+        // }
+        console.log('command is -----------: ' + command)
         processRef.stdin.write(`${command}
         `);
     })
+
+    socket.on("compile", async (code) => {
+        await writeFile(socket.id + ".c", code)
+        const processRef = cmd.get(`g++ -g ${socket.id}.c -o ${socket.id}`, function(err, data, stderr) {
+            if (stderr) {
+                socket.emit("result", stderr)
+            } else {
+                socket.emit("result", "Compilation successful!")
+            }
+            // fs.unlink(`${socket.id}.c`, (data, err) => {
+            // })
+        })
+    })
+
+    socket.on("run", async (code) => {
+        await writeFile(socket.id + ".c", code)
+        // if (debugStarted[socket.id]) {
+        //     socket.emit("error", "Please stop the debug session")
+        // }
+        const processRef = cmd.get(`g++ -g ${socket.id}.c -o ${socket.id}`, function(err, data, stderr) {
+            if (stderr) {
+                console.log(`-------------error----------------: ${stderr}`)
+                socket.emit("result", stderr)                
+            } else {
+                cmd.get(`${socket.id}.exe`, function(err, data, stderr) {
+                    socket.emit("result", data)
+                    // fs.unlink(`${socket.id}.exe`, (data, err) => {
+                    // })        
+                })
+            }
+            // fs.unlink(`${socket.id}.c`, (data, err) => {
+            // })
+        })
+    });
+    
 
     socket.emit("auth", socket.id)
 });
@@ -78,7 +123,7 @@ app.post("/compile", async (req, res) => {
 
 app.post("/run", async (req, res) => {
     await writeFile(req.body.id + ".c", req.body.code)
-    if (debugStarted[socket.id]) {
+    if (debugStarted[req.body.id]) {
         res.status(400).send("Please stop the debug session")
     }
     console.log("I received the run command")
@@ -93,6 +138,14 @@ app.post("/run", async (req, res) => {
     })
 });
 
-http.listen(8080, function(){
-  console.log('listening on http://localhost:8082');
+app.get('/', (req, res) => {
+    res.status(200).send("Hello, I'm done!");
 });
+
+http.listen(8081, function(){
+  console.log('listening on http://localhost:8081');
+});
+
+} catch (e) {
+    console.log(e)
+}
--- sockets/zEdgwv7RU1mVNPWRAAAD.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main() {
-    int x = 10;
-    printf("He is %d\n", x++);
-    int y = x;
-    printf("Y is: %d, iar x is: %d", y, x);
-}
